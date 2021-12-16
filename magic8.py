957f4bb53e6848c744f517f669b3a08e9507b99f
kiratreat <92818477+kiratreat@users.noreply.github.com>
2021-12-16 09:13:25 -0800

    Create magic8.py

---
+++ magic8.py
@@ -0,0 +1,38 @@
+# This is my first project that I've created on my journey to becomming a Software Engineer. Enjoy!
+
+import random
+
+name = ""
+question = ""
+answer = ""
+
+random_number = random.randint(1, 9)
+
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+else:
+  answer = "Error"
+if question == "":
+  answer = "You must ask a question to have your fortune told."
+
+if name == "":
+  print("Question: " + question)
+else:
+  print(name + "asks: " + question)
+print("Magic 8-ball's answer: " + answer)
