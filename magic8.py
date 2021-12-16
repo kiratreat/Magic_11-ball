882cf2a5309560b98bd6b2e4bc45d96b64d27b79
kiratreat <92818477+kiratreat@users.noreply.github.com>
2021-12-16 09:19:51 -0800

    Update magic8.py

--- magic8.py
+++ magic8.py
@@ -6,7 +6,7 @@ name = ""
 question = ""
 answer = ""
 
-random_number = random.randint(1, 9)
+random_number = random.randint(1, 11)
 
 if random_number == 1:
   answer = "Yes - definitely."
@@ -26,6 +26,10 @@ elif random_number == 8:
   answer = "Outlook not so good."
 elif random_number == 9:
   answer = "Very doubtful."
+elif random_number == 10:
+  answer = "The answer is easy... YES!"
+elif random_number == 11:
+  answer = "It's a yes in my book."
 else:
   answer = "Error"
 if question == "":
